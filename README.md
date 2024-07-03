e3bbf1e209ab4b72daf4b15ea9a200ca2a344e3a
Pablo González de la Parra <pablo.gonzalezdlp@gmail.com>
2024-07-03 16:53:40 -0600

    Added SDK and update README

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # Amigo-Imaginario-AI-
+
+## Create virtual environment
+```bash
+python -m venv venv
+```
+
+## Activate virtual environment
+```bash
+source venv/bin/activate
+```
+
+## Deactivate virtual environment
+```bash
+deactivate
+```
+
+## Install required libraries
+```bash
+pip install -r ./requirements.txt
+```
+
+## Run the project
+```bash
+python app.py
+```
--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,22 @@
-from flask import Flask,jsonify,request
+# Import libraries
+import os
+from flask import Flask, request, jsonify
+from firebase_admin import credentials, firestore, initialize_app # type: ignore
 
-
+# Initialize Flask App
 app = Flask(__name__)
+
+# Initialize Firestore DB
+cred = credentials.Certificate('key.json')
+default_app = initialize_app(cred)
+db = firestore.client()
+todo_ref = db.collection('todos')
 
 @app.route('/ping')
 def ping():
     return jsonify({"message":"pong"}),200
 
-
 if __name__ == "__main__":
     app.run(debug=True, port=8082)
+# app.py
+# Required Imports
