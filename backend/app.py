68ab89ef796718723e44a17bbbdee13786a29160
Pablo González de la Parra <pablo.gonzalezdlp@gmail.com>
2024-07-05 16:21:59 -0600

    Added model behaviour and chat history

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,8 @@
 # Import libraries
 import os
 from flask import Flask, request, jsonify
-from firebase_admin import credentials, firestore, initialize_app # type: ignore
-from dotenv import load_dotenv #type: ignore
+from firebase_admin import credentials, firestore, initialize_app  # type: ignore
+from dotenv import load_dotenv  # type: ignore
 from gemini import GeminiController
 
 # Load environment variables
@@ -12,21 +12,29 @@ load_dotenv()
 app = Flask(__name__)
 
 # Initialize Firestore DB
-cred = credentials.Certificate('key.json')
+cred = credentials.Certificate("key.json")
 default_app = initialize_app(cred)
 db = firestore.client()
-todo_ref = db.collection('todos')
+todo_ref = db.collection("todos")
 
-# Initialize object
+# Initialize gemini controller object
 gemini_controller = GeminiController()
 
-@app.route('/ping')
-def ping():
-    return jsonify({"message":"pong"}),200
 
-@app.route('/test_gemini', methods=['GET'])
+@app.route("/ping")
+def ping():
+    return jsonify({"message": "pong"}), 200
+
+
+@app.route("/test_gemini", methods=["GET"])
 def test_gemini():
-    return jsonify({"message": gemini_controller.generate_text("What is the meaning of life")}), 200
+    return (
+        jsonify(
+            {"message": gemini_controller.generate_text("What is the meaning of life")}
+        ),
+        200,
+    )
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=8082)
--- backend/gemini.py
+++ backend/gemini.py
@@ -9,14 +9,37 @@ class GeminiController:
         load_dotenv()
         GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
         genai.configure(api_key=GEMINI_API_KEY)
-        self.model = genai.GenerativeModel("gemini-1.5-flash")
+
+    # Generar modelo
+    def generate_chat_session(self, behaviour="", prevHistory=[]):
+        generation_config = {
+            "temperature": 1,
+            "top_p": 0.95,
+            "top_k": 64,
+            "max_output_tokens": 8192,
+            "response_mime_type": "text/plain",
+        }
+
+        model = genai.GenerativeModel(
+            model_name="gemini-1.5-flash",
+            generation_config=generation_config,
+            system_instruction=behaviour,
+        )
+
+        self.chat_session = model.start_chat(history=prevHistory)
 
     # Generar texto a partir de entradas de texto
-    def generate_text(self, prompt):
-        response = self.model.generate_content(prompt)
+    def generate_text(self, prompt, behaviour="", prevHistory=[]):
+        self.generate_chat_session(behaviour, prevHistory)
+        response = self.chat_session.send_message(prompt)
         return response.text
 
 
 if __name__ == "__main__":
+    # Ejemplo
     gemini_controller = GeminiController()
-    print(gemini_controller.generate_text("What is the meaning of life"))
+    print(
+        gemini_controller.generate_text(
+            "What is the meaning of life", "Behave as a scientist"
+        )
+    )
